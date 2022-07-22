71073028f0f85965fbab957af0537dd7aced65a2
Kry5t14n <savcio9182@gmail.com>
2022-07-22 15:00:11 +0200

    Updates

---
+++ precache-manifest.3345ad1bc71f161cbeaebe7320f5814b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "796cda00bbd00ea92b45",
+    "url": "/first-react-app/static/css/main.cb65058f.chunk.css"
+  },
+  {
+    "revision": "796cda00bbd00ea92b45",
+    "url": "/first-react-app/static/js/main.796cda00.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/first-react-app/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "bd37d1de6f4b162850cd",
+    "url": "/first-react-app/static/js/2.bd37d1de.chunk.js"
+  },
+  {
+    "revision": "99cfa6ba98f1959c8757",
+    "url": "/first-react-app/static/js/runtime~main.99cfa6ba.js"
+  },
+  {
+    "revision": "26f19ea56a2641a9fb1178bcd1e4f145",
+    "url": "/first-react-app/index.html"
+  }
+];
